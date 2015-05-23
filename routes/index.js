d0b74707b33d5548d10f28aeb336ecb98c918159
gpscasso <gpscasso@gmail.com>
2015-05-23 15:53:04 +0200

    Buscador de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,11 @@ router.get('/author', function(req, res){
 	res.render('author', {autor: 'Guillérmo Pages Scasso'});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload
+router.param('quizId', quizController.load);
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
